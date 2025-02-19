7cb8232ed90f40d1b373d8c3552b62599d21765f
danydodson <danydodson@gmail.com>
2025-02-19 05:41:45 -0600

    updated

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -2,13 +2,13 @@
 export default {
 	meta: {
 		name: 'Fluent',
-		author: 'Gibbu',
+		author: 'danydodson',
 		version: '1.0.10',
 		description: 'Brings the look of Windows 11 to Discord.',
-		source: 'https://github.com/DiscordStyles/Fluent',
+		source: 'https://github.com/danydodson/fluent',
 		invite: 'ZHthyCw',
 		authorId: '174868361040232448'
 	},
-	baseImport: 'https://discordstyles.github.io/Fluent/Fluent.css',
+	baseImport: 'https://raw.githubusercontent.com/danydodson/fluent/refs/heads/main/dist/Fluent.css',
 	addons: [['src/addons/_icons.scss', 'dist/Icons.css']]
 };
